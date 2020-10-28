fe5a1e652fc440989567399d541ce710084efb58
Nasir Khan <virgodgreat@gmail.com>
2020-10-28 18:58:39 +0500

    Add NotFound page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/Home.vue';
+import NotFound from '@/views/NotFound';
 import store from '@/store';
 
 Vue.use(VueRouter);
@@ -11,32 +12,22 @@ const routes = [
     name: 'Home',
     component: Home,
   },
-  // {
-  //   path: '/profile',
-  //   name: 'Profile',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ '@/views/Profile.vue'),
-  // },
   {
     path: '/auth/login',
     name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '@/views/Auth/Login.vue'),
   },
   {
     path: '/auth/register',
     name: 'Register',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '@/views/Auth/Register.vue'),
+  },
+  {
+    path: '*',
+    name: 'Not-Found',
+    component: NotFound,
   },
 ];
 
